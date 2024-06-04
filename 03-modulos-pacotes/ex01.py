36fdf3630a49fe5fbdd3f0de7e58fbcea93d86ca
Gizetto61 <127257041+Gizetto61@users.noreply.github.com>
2024-06-04 14:34:26 -0300

    Delete 03-modulos-pacotes/ex01.py

--- 03-modulos-pacotes/ex01.py
+++
@@ -1,9 +0,0 @@
-comp = float(input("Digite o Comprimento do Aquário: "))
-alt = float(input("Digite a Altura do Aquário: "))
-larg = float(input("Digite a Largura do Aquário: "))
-
-def volume(comp, alt, larg):
-    return (comp * alt * larg) / 1000
-
-def Termostato ():
-    
